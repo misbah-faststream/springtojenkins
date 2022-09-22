e0b7bb39f1b10cf2ba83d0e43a627742dedc69cd
misbah <misbah@faststream.com>
2022-09-22 05:30:51 +0530

    first commit

---
+++ src/main/java/com/demoapplication/mydemoapp/mydemopage.java
@@ -0,0 +1,13 @@
+package com.demoapplication.mydemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication    
+public class mydemopage {
+	public static void main(String[] args) {
+	SpringApplication.run(mydemopage.class, args); 
+	    System.out.println("Hello World!");
+	  }
+}
